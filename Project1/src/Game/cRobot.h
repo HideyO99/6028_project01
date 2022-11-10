d7e43e7a5b65a00de8cdcca869734a1ba261b11e
Siraphong <hideyo99@hotmail.com>
2022-11-10 00:35:00 -0500

    add enum weapon

--- Project1/src/Game/cRobot.h
+++ Project1/src/Game/cRobot.h
@@ -6,10 +6,17 @@ class cRobot
 public:
 	cRobot();
 	~cRobot();
-
+	enum weaponType
+	{
+		TYPE_LASER = 3,
+		TYPE_BOMB = 6,
+		TYPE_BULLETS = 9,
+		TYPE_ROCKET = 10
+	};
 	int id;
 	int health;
 	glm::vec3 position;
+	weaponType weaponType_;
 
 };
 
